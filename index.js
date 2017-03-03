900fc26cd361afe8f5c9c2fa187bfb8cb0e2cab3
Sebastien Lafleur <sebastien.lafleur@gmail.com>
2017-03-02 20:36:10 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({width: 800, height: 600});
+
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    win.on('closed', function () {
+        win = null
+    })
+}
+
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+});
+
+app.on('activate', function () {
+    if (win === null) {
+        createWindow()
+    }
+});
